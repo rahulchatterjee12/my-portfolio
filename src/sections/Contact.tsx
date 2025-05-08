9bfd81fe0cc71b33a2d13cd7dbc8e4832d60d3e3
Rahul Chatterjee <rc5292684@gmail.com>
2025-05-08 11:23:52 +0530

    add contact card

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,3 +1,38 @@
+import ArrowIcon from "../assets/icons/arrow-up-right.svg";
+import grainImg from "../assets/images/grain.jpg";
+
 export const ContactSection = () => {
-  return <div>Contact Section</div>;
+  return (
+    <div className="py-16 pt-14 lg:py-24 lg:pt-20">
+      <div className="container">
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl  text-center md:text-start relative overflow-hidden z-0">
+          <div
+            className="absolute inset-0 opacity-5 -z-10"
+            style={{
+              backgroundImage: `url(${grainImg.src})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          ></div>
+          <div className="flex flex-col md:flex-row md:justify-between items-center gap-8 md:gap-16">
+            <div>
+              <h2 className="font-serif text-2xl md:text-3xl">
+                Let's Create something amazing together.
+              </h2>
+              <p className="text-sm mt-2 md:text-base">
+                Ready to bring your next project to life? Let's connect and
+                discuess how can i help you achive your gole.
+              </p>
+            </div>
+            <div>
+              <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max">
+                <span className="font-semibold">Contact Me</span>
+                <ArrowIcon className="size-4" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
